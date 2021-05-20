bd4f7a04bb6ecf7b8273b3ca441edc38dd0fa23c
Bear <58261397+BearPessimist@users.noreply.github.com>
2021-05-20 11:51:26 +0800

    pull commit

--- src/main/java/com/bear/git/GitTest.java
+++ src/main/java/com/bear/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("Hello, World");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("pull test");
+        
     }
 }
